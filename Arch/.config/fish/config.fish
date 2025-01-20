784a627279e34c2d431ee806dd76405602337ceb
Oery <oery.dev@gmail.com>
2025-01-20 19:05:49 +0100

    Add Fish

---
+++ Arch/.config/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    set fish_greeting
+end
+
+clear
+
+starship init fish | source
+
+alias pamcan=pacman
+alias ls=lsd
+alias cls=clear
+alias hx=helix
+alias zed=zeditor
+
+alias flutter=/home/oery/Documents/Flutter/flutter/bin/flutter
+
+# BUN
+set BUN_INSTALL "$HOME/.bun"
+set PATH $BUN_INSTALL/bin:$PATH
+
+function postexec_test --on-event fish_postexec
+    if test "$argv" = cls
+    else if test "$argv" = clear
+    else
+        echo
+    end
+end
