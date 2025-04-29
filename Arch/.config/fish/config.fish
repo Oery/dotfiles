b3046d54dc8254c6425770b2727fa7dc5d5e441d
Oery <oery.dev@gmail.com>
2025-04-29 13:54:00 +0200

    Update fish config

--- Arch/.config/fish/config.fish
+++ Arch/.config/fish/config.fish
@@ -1,6 +1,20 @@
+# function fish_prompt -d "Write out the prompt"
+# This shows up as USER@HOST /home/user/ >, with the directory colored
+# $USER and $hostname are set by fish, so you can just use them
+# instead of using `whoami` and `hostname`
+#   printf '%s@%s %s%s%s > ' $USER $hostname \
+#   (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+# end
+
 if status is-interactive
+    # Commands to run in interactive sessions can go here
     set fish_greeting
+
 end
+
+# if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+#     cat ~/.cache/ags/user/generated/terminal/sequences.txt
+# end
 
 clear
 
@@ -25,3 +39,7 @@ function postexec_test --on-event fish_postexec
         echo
     end
 end
+
+zoxide init fish | source
+
+fish_add_path /home/oery/.millennium/ext/bin
